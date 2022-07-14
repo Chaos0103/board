d1bea699407ac125bb04dc8dc2d9a72a957625b2
chaos0103 <lyt1228@naver.com>
2022-07-15 01:03:57 +0900

    feat: 댓글 서비스 구현
    댓글 서비스 구현 완료

--- src/main/java/toyproject/board/domain/Comment.java
+++ src/main/java/toyproject/board/domain/Comment.java
@@ -1,6 +1,8 @@
 package toyproject.board.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
@@ -8,6 +10,7 @@ import static javax.persistence.FetchType.*;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment {
 
     @Id @GeneratedValue
@@ -20,10 +23,23 @@ public class Comment {
 
     @OneToOne(fetch = LAZY)
     @JoinColumn(name = "comment_id")
-    private Comment child;
+    private Comment parent;
 
     @Lob
     private String content;
     private Boolean anonymous;
     private int goodCount;
+
+    public Comment(Post post, Comment parent, String content, Boolean anonymous) {
+        this.post = post;
+        this.parent = parent;
+        this.content = content;
+        this.anonymous = anonymous;
+        this.goodCount = 0;
+    }
+
+    //==비즈니스 로직==//
+    public void addGoodCount() {
+        this.goodCount++;
+    }
 }
